b5df4b57f58716b92cb8f4a9e702d91b08b4f5b8
Vinicios Ziroldo Gomes <ziroldovi@gmail.com>
2022-09-04 14:51:02 -0300

    Initial Commit

---
+++ WebCRUDMVCSQL/Data/MVC_Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebCRUDMVCSQL.Models;
+
+namespace WebCRUDMVCSQL.Data
+{
+    public class MVC_Context : DbContext
+    {
+        public MVC_Context(DbContextOptions<MVC_Context> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produto { get; set; }    
+    }
+}
---
+++ WebCRUDMVCSQL/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebCRUDMVCSQL.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Column("Id")]
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+    }
+}
